fb2d30cf435086bb48e7c61dbe3b63d5f3ed6510
Lorn Jaeger <lornjaeger@proton.me>
2025-07-18 11:27:45 -0600

    theme

--- nvim/init.lua
+++ nvim/init.lua
@@ -104,17 +104,6 @@ require('lazy').setup({
   },
 
   {
-    'nvimdev/dashboard-nvim',
-    event = 'VimEnter',
-    config = function()
-      require('dashboard').setup {
-        -- config
-      }
-    end,
-    dependencies = { { 'nvim-tree/nvim-web-devicons' } },
-  },
-
-  {
     'akinsho/toggleterm.nvim',
     version = '*',
     config = function()
