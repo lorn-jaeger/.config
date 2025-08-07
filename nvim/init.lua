029886275fce4477ac4c2f37f6d23cf438093e65
Lorn Jaeger <lornjaeger@proton.me>
2025-08-07 12:17:41 -0600

    some changes

--- nvim/init.lua
+++ nvim/init.lua
@@ -894,7 +894,7 @@ require('lazy').setup({
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'modus_vivendi'
+      vim.cmd.colorscheme 'modus_operandi'
     end,
   },
 
